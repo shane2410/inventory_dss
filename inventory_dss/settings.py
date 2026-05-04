a68bad3d4b825f9f3ad57c7b6c2737c3af7887e8
Pham Quang Khiem <khiemq885gmail.com>
2026-05-04 14:54:54 +0700

    Allow any Render subdomain in ALLOWED_HOSTS

--- inventory_dss/settings.py
+++ inventory_dss/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Hosts: include Render default for service named "stratix" plus localhost defaults
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'stratix.onrender.com,127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com,127.0.0.1,localhost').split(',')
 
 # Show a user-friendly message when CSRF token is stale/invalid.
 CSRF_FAILURE_VIEW = 'inventory.views.csrf_failure'
